b7b00e22e640fdfa1864015719605a4e50ad9e1f
Troels Ugilt Jensen <tujensen@gmail.com>
2015-03-20 12:27:16 +0100

    Issue #KOBA-102 by tuj: Removed wayf bundle from Kernel and config.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel {
       new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
       new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
       new Koba\BasicBookingBundle\KobaBasicBookingBundle(),
-      new Itk\WayfBundle\ItkWayfBundle(),
+      //new Itk\WayfBundle\ItkWayfBundle(),
     );
 
     if (in_array($this->getEnvironment(), array('dev', 'test'))) {
